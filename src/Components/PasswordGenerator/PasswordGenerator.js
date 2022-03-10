8fa8747595f845f48fe17a3f89afd5cdf0638181
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-03-10 17:02:13 +0000

    Adding copy to clipboard

--- src/Components/PasswordGenerator/PasswordGenerator.js
+++ src/Components/PasswordGenerator/PasswordGenerator.js
@@ -1,41 +1,60 @@
-import css from './PasswordGenerator.module.css';
-import { generatePassword } from './generatePassword';
-import { Button, Typography } from '@mui/material';
-import { useState } from 'react';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import css from "./PasswordGenerator.module.css";
+import { generatePassword } from "./generatePassword";
+import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { useDispatch, useSelector } from "react-redux";
+import { setPassword } from "../../Store/slices/passwordSlices";
+import { copyToClipboard } from "../../Utils/copyToClipboard";
 
 const PasswordResult = (props) => {
-    const password = generatePassword(12);
+  const password = useSelector((state) => state.password.password);
 
-    return (
-        <div className={css.Result}>
-            <Typography variant='body1'>
-                {password}
-            </Typography>
-        </div>
-    )
-}
+  return (
+    <div className={css.Result}>
+      <Typography variant="body1">{password}</Typography>
+    </div>
+  );
+};
+
+const CopyToClipboard = () => {
+  const password = useSelector((state) => state.password.password);
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        copyToClipboard(password);
+      }}
+      startIcon={<ContentCopyIcon />}
+    >
+      Copy to clipboard
+    </Button>
+  );
+};
 
 const PasswordGenerator = () => {
-    const [sessionID, updateSessionID] = useState(0);
-    
-    return (
-        <div className={css.PasswordGenerator}>
-            <Typography variant="h2" component="h2">
-                Recommended Password
-            </Typography>
-            <PasswordResult session={sessionID}/>
-            <Button 
-                variant="outlined" 
-                onClick={() => {
-                    updateSessionID(sessionID + 1);
-                }}
-                startIcon={<RefreshIcon />}
-            >
-                Generate New Password
-            </Button>
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+
+  return (
+    <div className={css.PasswordGenerator}>
+      <Typography variant="h2" component="h2">
+        Recommended Password
+      </Typography>
+      <PasswordResult />
+      <CopyToClipboard />
+      <Button
+        variant="outlined"
+        onClick={() => {
+          dispatch(setPassword(generatePassword(12)));
+        }}
+        startIcon={<RefreshIcon />}
+      >
+        Generate New Password
+      </Button>
+    </div>
+  );
+};
 
 export default PasswordGenerator;
