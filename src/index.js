abe4181de0be2fad912cbe5ce2b728c4c2019e0d
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-08-28 15:05:08 +0100

    #19 React 18 setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./Store/store";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
